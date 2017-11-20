c1f4bafc3432d2f42d913783a2cb3a7df4ad25e1
ambrosiae <yang.vanessa@outlook.com>
2017-11-20 11:39:34 -0800

    Replaced files with starter theme files

--- themes/inhabitent/template-parts/content.php
+++ themes/inhabitent/template-parts/content.php
@@ -8,26 +8,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<section class="site-area">
-	<div class="blog-content">
 	<header class="entry-header">
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
 
-		<?php the_title( sprintf( '<a class="entry-title" href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-
-	<?php if ( 'post' === get_post_type() ) : ?>
+		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_posted_by(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
+	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
-	</div><!-- entry-content -->
-	</div><!-- blog-content -->
-</section><!--site-area-->
-</article> <!--post-##-->
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
