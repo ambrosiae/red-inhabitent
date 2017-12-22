9c71aa0fa7adf4491ca7213f5030dc6305a835f0
ambrosiae <yang.vanessa@outlook.com>
2017-12-21 20:45:01 -0800

    Styled journal page, contact page, shop and journal sections on front page

--- themes/inhabitent/js/nav.js
+++ themes/inhabitent/js/nav.js
@@ -1,12 +1,13 @@
 (function ($){
     $('#nav-toggle').on('click', function() {
       event.preventDefault();
-      $('.search-field').animate({width: "230px"}, 800).focus();
+      $('.search-field').animate({width: "180px"}, 500).focus();
     });
 
     $('.search-field').on('blur', function() {
       event.preventDefault();
-      $('.search-field').animate({width: "0px"}, 800).focus();
+      $(this).animate({width: "0px"}, 500).focus();
+      $(this).css('background', 'rgba(255,255,255,0.3)');
     });
 
       $('.search-field').animate({width: 0}, 0);
